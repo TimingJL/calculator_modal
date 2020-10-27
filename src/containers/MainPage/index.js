f883a73b523f1070b521e620b471a947af67cf78
taiming <taiming@bridgewell.com>
2020-10-27 17:40:29 +0800

    [feature] add button

--- src/containers/MainPage/index.js
+++ src/containers/MainPage/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const MainPage = () => <div>MainPage</div>;
+const MainPage = () => (
+  <div>
+    <button type="button">Calculator Modal</button>
+  </div>
+);
 
 export default MainPage;
