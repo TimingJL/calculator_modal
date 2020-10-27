1d3d11da735a5509241194623af0d01e26b21a7c
taiming <taiming@bridgewell.com>
2020-10-28 07:59:06 +0800

    [feature] styling CircleButton

--- src/components/Calculator/CircleButton.js
+++ src/components/Calculator/CircleButton.js
@@ -3,26 +3,41 @@ import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
-  root: (props) => ({
-    color: props.color,
-    background: props.background,
+  root: {
     height: "100%",
     width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: (props) => ({
+    color: props.color,
+    background: props.background,
+    height: "calc(100% - 16px)",
+    width: "calc(100% - 16px)",
     borderRadius: 50,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     cursor: "pointer",
-    fontSize: 48,
+    fontSize: 36,
+    userSelect: "none",
     "&:hover": {
       opacity: 0.8,
+    },
+    "&:active": {
+      opacity: 0.5,
     },
   }),
 }));
 
 const CircleButton = ({ text, color, background }) => {
   const classes = useStyles({ color, background });
-  return <div className={classes.root}>{text}</div>;
+  return (
+    <div className={classes.root}>
+      <span className={classes.button}>{text}</span>
+    </div>
+  );
 };
 
 CircleButton.propTypes = {
