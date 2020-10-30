28fa9df5ac575de5cc2df457964afe2b9f7cd78e
taiming <taiming@bridgewell.com>
2020-10-30 23:38:32 +0800

    [refactor] refactor buttons grid

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import Numbers from "./Numbers";
-import ArithmeticOperators from "./ArithmeticOperators";
-import OtherOperators from "./OtherOperators";
 import DisplayBoard from "./DisplayBoard";
+import ButtonGrid from "./ButtonsGrid";
 
 const useStyles = makeStyles((theme) => {
   const {
@@ -20,27 +18,6 @@ const useStyles = makeStyles((theme) => {
       display: "flex",
       flexDirection: "column",
     }),
-    gridContainer: (props) => ({
-      width: props.fullWidth ? "100vw" : props.width,
-      height: props.fullWidth ? "125vw" : props.width * 1.25,
-      padding: 8,
-      boxSizing: "border-box",
-    }),
-    buttonsGrid: () => ({
-      width: "100%",
-      height: "100%",
-
-      display: "grid",
-      gridTemplateColumns: "repeat(4, 1fr)",
-      gridTemplateRows: "repeat(5, 1fr)",
-      gridTemplateAreas: `
-        "otherOperator otherOperator otherOperator arithmeticOperator"
-        "numbers numbers numbers arithmeticOperator"
-        "numbers numbers numbers arithmeticOperator"
-        "numbers numbers numbers arithmeticOperator"
-        "numbers numbers numbers arithmeticOperator"
-      `,
-    }),
   };
 });
 
@@ -51,13 +28,11 @@ const Calculator = ({ fullWidth, value, operator, handleOnClick }) => {
   return (
     <div className={classes.root}>
       <DisplayBoard value={value} operator={operator} />
-      <div className={classes.gridContainer}>
-        <div className={classes.buttonsGrid}>
-          <OtherOperators handleOnClick={handleOnClick} />
-          <Numbers handleOnClick={handleOnClick} />
-          <ArithmeticOperators handleOnClick={handleOnClick} />
-        </div>
-      </div>
+      <ButtonGrid
+        fullWidth={fullWidth}
+        matches={matches}
+        handleOnClick={handleOnClick}
+      />
     </div>
   );
 };
